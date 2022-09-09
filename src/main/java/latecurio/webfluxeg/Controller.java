6855f767ace88ce44df6b67735fb88351621c566
twcrone <twcrone@gmail.com>
2022-09-09 15:59:18 -0400

    Checkpoint...

--- src/main/java/latecurio/webfluxeg/Controller.java
+++ src/main/java/latecurio/webfluxeg/Controller.java
@@ -1,8 +1,11 @@
 package latecurio.webfluxeg;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -65,5 +68,15 @@ public class Controller {
     @GetMapping("/some10")
     public Mono<String> some10() {
         return Mono.just("10");
+    }
+
+    private WebClient webClient = WebClient.create();
+
+    @GetMapping("/doeet/{ts}")
+    public Flux<String> doeet(@PathVariable String ts) {
+        return Flux.just("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+                .map(it -> webClient.get().uri("http://example.com?i=%s&ts=%i", it, System.currentTimeMillis()))
+                .map(response -> response.retrieve().bodyToMono(String.class))
+                .flatMap(it -> it);
     }
 }
