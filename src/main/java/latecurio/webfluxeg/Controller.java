3caddc92a4f51317e3ccd67131e0613544c28b58
twcrone <twcrone@gmail.com>
2022-09-09 15:38:21 -0400

    Checkpoint...

--- src/main/java/latecurio/webfluxeg/Controller.java
+++ src/main/java/latecurio/webfluxeg/Controller.java
@@ -8,8 +8,62 @@ import reactor.core.publisher.Mono;
 @RestController
 public class Controller {
 
-    @GetMapping("/hello/{name}")
-    public Mono<String> sayHello(@PathVariable String name) {
-        return Mono.just("Hello " + name);
+    @GetMapping("/some1")
+    public Mono<String> some1() {
+        return Mono.just("1");
+    }
+
+
+    @GetMapping("/some2")
+    public Mono<String> some2() {
+        return Mono.just("2");
+    }
+
+
+    @GetMapping("/some3")
+    public Mono<String> some3() {
+        return Mono.just("3");
+    }
+
+
+    @GetMapping("/some4")
+    public Mono<String> some4() {
+        return Mono.just("4");
+    }
+
+
+    @GetMapping("/some5")
+    public Mono<String> some5() {
+        return Mono.just("5");
+    }
+
+
+    @GetMapping("/some6")
+    public Mono<String> some6() {
+        return Mono.just("6");
+    }
+
+
+    @GetMapping("/some7")
+    public Mono<String> some7() {
+        return Mono.just("7");
+    }
+
+
+    @GetMapping("/some8")
+    public Mono<String> some8() {
+        return Mono.just("8");
+    }
+
+
+    @GetMapping("/some9")
+    public Mono<String> some9() {
+        return Mono.just("9");
+    }
+
+
+    @GetMapping("/some10")
+    public Mono<String> some10() {
+        return Mono.just("10");
     }
 }
---
+++ test.sh
@@ -0,0 +1,10 @@
+http localhost:8080/some1 &
+http localhost:8080/some2 &
+http localhost:8080/some3 &
+http localhost:8080/some4 &
+http localhost:8080/some5 &
+http localhost:8080/some6 &
+http localhost:8080/some7 &
+http localhost:8080/some8 &
+http localhost:8080/some9 &
+http localhost:8080/some10 &
