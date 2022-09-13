645edcea31cef34d0e4a13871b80e28dedd87c8a
twcrone <twcrone@gmail.com>
2022-09-13 11:40:36 -0400

    Get basic test container working

--- src/test/java/latecurio/webfluxeg/WebfluxegApplicationTests.java
+++ src/test/java/latecurio/webfluxeg/WebfluxegApplicationTests.java
@@ -2,9 +2,19 @@ package latecurio.webfluxeg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class WebfluxegApplicationTests {
+
+//	@ClassRule
+//	public static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8");
+
+	@Container
+	public GenericContainer mySQLContainer = new GenericContainer("mysql").withExposedPorts(3306);
 
 	@Test
 	void contextLoads() {
